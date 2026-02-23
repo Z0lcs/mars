aaab85e8986fc941963e0075482011df700394a3
Zolcs <aranyzoli11@gmail.com>
2026-02-23 15:34:47 +0100

    Rover, Map osztály

---
+++ Vadász-Mars=Dénes/MarsMap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Vadász_Mars_Dénes
+{
+
+    public class MarsMap
+    {
+        public string[][] Grid { get; private set; } = new string[50][];
+        public List<Point> Akadalyok { get; } = new();
+        public List<Point> RitkaArany { get; } = new();
+        public List<Point> RitkaAsvany { get; } = new();
+        public List<Point> Vizjeg { get; } = new();
+        public Point KezdoPont { get; private set; }
+
+        public void LoadFromFile(string path)
+        {
+            using StreamReader sr = new(path);
+            int sor = 0;
+            while (!sr.EndOfStream && sor < 50)
+            {
+                string[] parts = sr.ReadLine().Split(',');
+                for (int oszlop = 0; oszlop < parts.Length; oszlop++)
+                {
+                    Point p = new Point(sor, oszlop);
+                    switch (parts[oszlop])
+                    {
+                        case "#": Akadalyok.Add(p); break;
+                        case "Y": RitkaArany.Add(p); break;
+                        case "B": Vizjeg.Add(p); break;
+                        case "G": RitkaAsvany.Add(p); break;
+                        case "S": KezdoPont = p; break;
+                    }
+                }
+                Grid[sor] = parts;
+                sor++;
+            }
+        }
+    }
+}
--- Vadász-Mars=Dénes/Program.cs
+++ Vadász-Mars=Dénes/Program.cs
@@ -4,51 +4,15 @@
     {
         static void Main(string[] args)
         {
-            string path = @"C:\mars\mars_map_50x50.csv";
-            string[][] map = new string[50][];
-            StreamReader sr = new(path);
+            MarsMap terkep = new MarsMap();
+            string path = "mars_map_50x50.csv"; 
+            terkep.LoadFromFile(path);
 
-            List<string> akadaly = new List<string>();
-            List<string> ritkaArany = new List<string>();
-            List<string> ritkaAsvany = new List<string>();
-            List<string> vizjeg = new List<string>();
-            string kezdoPont = "";
+            Rover rover = new Rover();
+            rover.Pozicio = terkep.KezdoPont;
 
-            int sorszam = 0;
-            while (!sr.EndOfStream && sorszam < 50)
-            {
-                string line = sr.ReadLine();
-                string[] parts = line.Split(',');
-
-                for (int i = 0; i < parts.Length; i++)
-                {
-                    if (parts[i] == "#")
-                    {
-                        akadaly.Add($"{sorszam},{i}");
-                    }
-                    else if (parts[i] =="Y")
-                    {
-                        ritkaArany.Add($"{sorszam},{i}");
-                    }
-                    else if (parts[i] == "B")
-                    {
-                        vizjeg.Add($"{sorszam},{i}");
-                    }
-                    else if (parts[i] == "G")
-                    {
-                        ritkaAsvany.Add($"{sorszam},{i}");
-                    }
-                    else if (parts[i] == "S")
-                    {
-                        kezdoPont += $"{sorszam},{i}";
-                    }
-                    //Console.Write(parts[i] + " ");
-                }
-                map[sorszam] = parts;
-                sorszam++;
-                //Console.WriteLine();
-            }
-            sr.Close();
+            Console.WriteLine($"A rover elindult a ({rover.Pozicio.X},{rover.Pozicio.Y}) pontról.");
+            Console.WriteLine($"Találtunk {terkep.RitkaArany.Count+ terkep.RitkaAsvany.Count+ terkep.Vizjeg.Count} ásványt és {terkep.Akadalyok.Count} akadályt.");
 
         }
     }
